71098bcb0daed26f9b0cd43fde665f12eea0f543
Kinesh Lohar <kineshmalviya@gmail.com>
2025-06-15 14:22:39 +0530

    2. Refactor UI components and styles
    - Updated button styles to enhance appearance and interaction, including size adjustments and hover effects.
    - Introduced new UI components: Button, Input, Progress, and Textarea in the Home page.
    - Modified global styles in `globals.css` for consistent radius and color settings.
    - Adjusted layout to utilize the new DM Sans font for improved typography.
    - Enhanced accessibility and responsiveness across input components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div className=" text-rose-500">
-      Hello
+    <div className=" flex flex-col gap-8 items-start justify-center p-4">
+
+      <Button variant='elevated'>I am Button</Button>
+      <Input placeholder="sdjfsdjkfsd" />
+      <Progress value={69} />
+      <Textarea placeholder="sadadasdasd" />
     </div>
   );
 }
